00cab8798055a55f8cbe97bb87ba204d1841fec0
Guilherme <gfs8@duke.edu>
2020-03-05 13:11:23 -0500

    Deleted repeated files

--- dist/reptimer-0.0.1/reptimer/main.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-import subprocess
-import time
-
-
-def timer(seconds):
-    time.sleep(seconds)
-    subprocess.run(["afplay", "/System/Library/Sounds/Tink.aiff", "-v", "20"])
